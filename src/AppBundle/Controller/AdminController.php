47c55abb5cc496310ef2f1c394aec5077ffdf6dd
fernando <lujano14.93@gmail.com>
2018-01-31 01:03:17 -0600

    interaccion con base dedatos

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Translation\TranslatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\User;
+
 
 class AdminController extends Controller
 {
@@ -39,8 +42,61 @@ class AdminController extends Controller
 
    public function indexDashBoard(){
     
-      return $this->render("layout.admin.html.twig",[]);
+      return $this->render("admin/dashboard.html.twig",[]);
    }
+
+  /**
+   * @Route("/admin/signup")
+   */
+
+   public function indexSignup(){
+    
+      return $this->render("admin/signup.html.twig",[]);
+   }
+
+
+   /**
+   *@Route("/admin/create")
+   */
+
+   public function indexCreate(){
+     $em = $this->getDoctrine()->getManager();
+     $user= new User();
+
+     $user->setNombre("fernando");
+     $user->setPaterno("lujano");
+     $user->setMaterno("gutierrez");
+     $user->setCorreo("lujano14.93@gmail.com");
+     $user->setcontrasena("QQ");
+     $user->setNickname("lujano144");
+     $user->setTelefono("gutierrez");
+     $user->setMontoInicial(150.32);
+     $user->setCp("57500");
+     $user->setPais("57500");
+     
+      
+      $em->persist($user);
+      $em->flush();
+      return new Response("Creado usuario." . $user->getId());
+   }
+
+    /**
+    *@Route("/admin/show/{id}", defaults= {"id" =1 })
+    */
+   public function indexShow($id){
+     $repository = $this->getDoctrine()->getRepository( User::class );
+
+     $user= $repository->find($id);
+      
+      if(!$user){
+        throw $this->CreateNotFoundException("No user found for id ". $id);
+      }
+      
+      return new Response("usuario encontrado ." . $user->getId());
+   }
+
+
+
 
 
 }
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,415 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="UsuarioCliente")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=128, nullable=true)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido_paterno", type="string", length=128)
+     */
+    private $paterno;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido_materno", type="string", length=128, nullable=true)
+     */
+    private $materno;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="correo", type="string", length=256, unique=true)
+     */
+    private $correo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contrasena", type="string", length=512)
+     */
+    private $contrasena;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nickname", type="string", length=100, nullable=true, unique=true)
+     */
+    private $nickname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="telefono", type="string", length=12, nullable=true)
+     */
+    private $telefono;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="monto_inicial", type="decimal", precision=19, scale=4, nullable=true)
+     */
+    private $montoInicial;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="codigo_postal", type="string", length=12)
+     */
+    private $cp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pais", type="string", length=100, nullable=true)
+     */
+    private $pais;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_registro", type="datetime")
+     */
+    private $fechaRegistro;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return User
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set paterno
+     *
+     * @param string $paterno
+     *
+     * @return User
+     */
+    public function setPaterno($paterno)
+    {
+        $this->paterno = $paterno;
+
+        return $this;
+    }
+
+    /**
+     * Get paterno
+     *
+     * @return string
+     */
+    public function getPaterno()
+    {
+        return $this->paterno;
+    }
+
+    /**
+     * Set materno
+     *
+     * @param string $materno
+     *
+     * @return User
+     */
+    public function setMaterno($materno)
+    {
+        $this->materno = $materno;
+
+        return $this;
+    }
+
+    /**
+     * Get materno
+     *
+     * @return string
+     */
+    public function getMaterno()
+    {
+        return $this->materno;
+    }
+
+    /**
+     * Set correo
+     *
+     * @param string $correo
+     *
+     * @return User
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+
+        return $this;
+    }
+
+    /**
+     * Get correo
+     *
+     * @return string
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * Set contrasena
+     *
+     * @param string $contrasena
+     *
+     * @return User
+     */
+    public function setContrasena($contrasena)
+    {
+        $this->contrasena = $contrasena;
+
+        return $this;
+    }
+
+    /**
+     * Get contrasena
+     *
+     * @return string
+     */
+    public function getContrasena()
+    {
+        return $this->contrasena;
+    }
+
+    /**
+     * Set nickname
+     *
+     * @param string $nickname
+     *
+     * @return User
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname
+     *
+     * @return string
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param string $telefono
+     *
+     * @return User
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return string
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set montoInicial
+     *
+     * @param string $montoInicial
+     *
+     * @return User
+     */
+    public function setMontoInicial($montoInicial)
+    {
+        $this->montoInicial = $montoInicial;
+
+        return $this;
+    }
+
+    /**
+     * Get montoInicial
+     *
+     * @return string
+     */
+    public function getMontoInicial()
+    {
+        return $this->montoInicial;
+    }
+
+    /**
+     * Set cp
+     *
+     * @param string $cp
+     *
+     * @return User
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get cp
+     *
+     * @return string
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set pais
+     *
+     * @param string $pais
+     *
+     * @return User
+     */
+    public function setPais($pais)
+    {
+        $this->pais = $pais;
+
+        return $this;
+    }
+
+    /**
+     * Get pais
+     *
+     * @return string
+     */
+    public function getPais()
+    {
+        return $this->pais;
+    }
+
+    /**
+     * Set fechaRegistro
+     *
+     * @param \DateTime $fechaRegistro
+     *
+     * @return User
+     */
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaRegistro
+     *
+     * @return \DateTime
+     */
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+
+
+   /**
+     * @ORM\PrePersist
+     */
+    public function setCreaFechaRegistro()
+    {
+        $this->fechaRegistro = new \DateTime();
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function encriptarContrasena()
+    {
+            // PHP has had a built-in password_hash function since 5.5.0
+            // It's currently based on BCRYPT, but pass PASSWORD_BCRYPT
+            // to ensure this doesn't change.
+
+            // Generate a password using a random salt
+            $this->contrasena= password_hash($this->contrasena, PASSWORD_BCRYPT);
+
+            // Generate a password with a known salt.
+            //password_hash($password, PASSWORD_BCRYPT, array("salt" => $salt));
+
+
+            // Before 5.5:
+            // 5.3.7 and on: use $2y$ as the salt prefix
+            // Otherwise, use $2x$
+            // This will cause crypt to generate a bcrypt hash
+            //$salt = '$2y$10$' . mcrypt_create_iv(22);
+           // $salted_password = crypt($password, $salt)
+
+            // Both algorithms generate a 60-character string that looks like:
+// $salt . $hashed_password
+
+    }
+
+}
+
